cebf2717a1468d0cdf3d4014d6ddd44a96d77187
wilsonia <i@nwilson.dev>
2021-08-22 20:32:24 -0700

    xo style adherence

--- src/spins.js
+++ src/spins.js
@@ -1,30 +1,33 @@
-import { sqrt, complex, matrix, kron, transpose, multiply, add } from 'mathjs'
+import {sqrt, complex, matrix, kron, transpose, multiply, add} from 'mathjs';
 
-// Naming relevant objects
-	// Eigenstates
-	const z_up_bra = matrix([[1, 0]])
-	const z_down_bra = matrix([[0, 1]])
-	const z_up_ket = transpose(z_up_bra)
-	const z_down_ket = transpose(z_down_bra)
+// Eigenstates
+const zUpBra = matrix([[1, 0]]);
+const zDownBra = matrix([[0, 1]]);
+const zUpKet = transpose(zUpBra);
+const zDownKet = transpose(zDownBra);
 
-	const x_up_bra = matrix([1/sqrt(2), 1/sqrt(2)])
-	const x_down_bra = matrix([1/sqrt(2), -1/sqrt(2)])
-	const x_up_ket = transpose(x_up_bra)
-	const x_down_ket = transpose(x_down_bra)
+const xUpBra = matrix([1 / sqrt(2), 1 / sqrt(2)]);
+const xDownBra = matrix([1 / sqrt(2), -1 / sqrt(2)]);
+const xUpKet = transpose(xUpBra);
+const xDownKet = transpose(xDownBra);
 
-	const y_up_bra = matrix([1/sqrt(2), complex(0,1/sqrt(2))])
-	const y_down_bra = matrix([1/sqrt(2), complex(0,-1/sqrt(2))])
-	const y_up_ket = transpose(y_up_bra)
-	const y_down_ket = transpose(y_down_bra)
+const yUpBra = matrix([1 / sqrt(2), complex(0, 1 / sqrt(2))]);
+const yDownBra = matrix([1 / sqrt(2), complex(0, -1 / sqrt(2))]);
+const yUpKet = transpose(yUpBra);
+const yDownKet = transpose(yDownBra);
 
-	// Projectors
-	const p_z_up = kron(z_up_bra, z_up_ket)
-	const p_z_down = kron(z_down_bra, z_down_ket)
+// Projectors
+const zUpProj = kron(zUpBra, zUpKet);
+const zDownProj = kron(zDownBra, zDownKet);
+
+const xUpProj = kron(xUpBra, xUpKet);
+const xDownProj = kron(xDownBra, xDownKet);
+
+const yUpProj = kron(yUpBra, yUpKet);
+const yDownProj = kron(yDownBra, yDownKet);
 
 /*
    Leaving the oven, the electron's initial spin state is effectively random due to ignorance of its history.
    This is best represented by a mixed-state density matrix
 */
-var density_operator =  add(multiply(p_z_up, 1/2), multiply(p_z_down, 1/2))
-
-console.log(density_operator)
+let densityOperator =  add(multiply(zUpProj, 1/2), multiply(zDownProj, 1/2))
