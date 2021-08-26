c3c9b9a062cab1190491436edb585a48061b5cdc
wilsonia <i@nwilson.dev>
2021-08-26 14:48:40 -0700

    Born rule producing a scalar

--- src/spins.js
+++ src/spins.js
@@ -1,30 +1,30 @@
-import {sqrt, complex, matrix, kron, transpose, multiply, add} from 'mathjs';
+import {sqrt, complex, matrix, kron, ctranspose, multiply, add, trace, re} from 'mathjs';
 
 // Eigenstates
 const zUpBra = matrix([[1, 0]]);
 const zDownBra = matrix([[0, 1]]);
-const zUpKet = transpose(zUpBra);
-const zDownKet = transpose(zDownBra);
+const zUpKet = ctranspose(zUpBra);
+const zDownKet = ctranspose(zDownBra);
 
-const xUpBra = matrix([1 / sqrt(2), 1 / sqrt(2)]);
-const xDownBra = matrix([1 / sqrt(2), -1 / sqrt(2)]);
-const xUpKet = transpose(xUpBra);
-const xDownKet = transpose(xDownBra);
+const xUpBra = matrix([[1 / sqrt(2), 1 / sqrt(2)]]);
+const xDownBra = matrix([[1 / sqrt(2), -1 / sqrt(2)]]);
+const xUpKet = ctranspose(xUpBra);
+const xDownKet = ctranspose(xDownBra);
 
-const yUpBra = matrix([1 / sqrt(2), complex(0, 1 / sqrt(2))]);
-const yDownBra = matrix([1 / sqrt(2), complex(0, -1 / sqrt(2))]);
-const yUpKet = transpose(yUpBra);
-const yDownKet = transpose(yDownBra);
+const yUpBra = matrix([[1 / sqrt(2), complex(0, 1 / sqrt(2))]]);
+const yDownBra = matrix([[1 / sqrt(2), complex(0, -1 / sqrt(2))]]);
+const yUpKet = ctranspose(yUpBra);
+const yDownKet = ctranspose(yDownBra);
 
 // Projectors
-const zUpProj = kron(zUpBra, zUpKet);
-const zDownProj = kron(zDownBra, zDownKet);
+const zUpProj = kron(zUpKet, zUpBra);
+const zDownProj = kron(zDownKet, zDownBra);
 
-const xUpProj = kron(xUpBra, xUpKet);
-const xDownProj = kron(xDownBra, xDownKet);
+const xUpProj = kron(xUpKet, xUpBra);
+const xDownProj = kron(xDownKet, xDownBra);
 
-const yUpProj = kron(yUpBra, yUpKet);
-const yDownProj = kron(yDownBra, yDownKet);
+const yUpProj = kron(yUpKet, yUpBra);
+const yDownProj = kron(yDownKet, yDownBra);
 
 /*
    Leaving the oven, the electron's initial spin state is effectively random due to ignorance of its history.
@@ -32,9 +32,13 @@ const yDownProj = kron(yDownBra, yDownKet);
 */
 let densityOperator = add(multiply(zUpProj, 1 / 2), multiply(zDownProj, 1 / 2));
 
-// Born Rule for a quantum history
+// Born Rule for a quantum history (passed as a branch wavefunction)
 function probability(history) {
-	return multiply(transpose(history), densityOperator, history);
+	return re(trace(multiply(ctranspose(history), densityOperator, history)));
 }
 
-console.log(probability(yDownKet));
+// Example: measure Sz, then Sx
+// density operator is initialized with t1 event, now adding t2
+
+
+console.log(probability(multiply(yDownProj, zUpProj)));
