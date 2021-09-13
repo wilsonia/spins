437fad2489e0304118d922412afbe6a894c0f059
wilsonia <i@nwilson.dev>
2021-09-13 15:20:48 -0700

    build prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/gui.js',
-	mode: 'development',
+	mode: 'production',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
